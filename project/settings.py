58dbdbe592b8a03128b7eef97f3b44baa4f05fc6
Mohamad anas fattoum <samaanas30@gmail.com>
2025-01-31 20:27:43 +0100

    add apps in settings and created urls.py

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #installed apps
+    'rest-framework',
+    # My Apps
+    'filmverwaltungAPI',
 ]
 
 MIDDLEWARE = [
